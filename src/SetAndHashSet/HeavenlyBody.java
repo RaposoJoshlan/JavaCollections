d4f919c4fff56f1d1ef890127985e2e64c7bcb21
RaposoJoshlan <raposojoshlan2000@outlook.com>
2022-04-05 12:43:32 +0100

    Sets and hashset example

---
+++ src/SetAndHashSet/HeavenlyBody.java
@@ -0,0 +1,36 @@
+package SetAndHashSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created on 05/04/2022
+ */
+
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(satellites);
+    }
+}
---
+++ src/SetAndHashSet/Main.java
@@ -0,0 +1,66 @@
+package SetAndHashSet;
+
+import java.util.*;
+
+/**
+ * Created on 05/04/2022
+ */
+
+public class Main {
+
+    /**
+     * Sets have no order defined and a set cannot contain duplicates
+     * HashSet a subtype of Set interface
+     * There is no way to retrieve an item from a set. It only permits if the item exists
+     * */
+
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
+    public static void main(String[] args) {
+        HeavenlyBody temp = new HeavenlyBody("Mercury", 88);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        HeavenlyBody tempMoon = new HeavenlyBody("Moon of Mercury", 27);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+
+        temp = new HeavenlyBody("Venus", 225);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        tempMoon = new HeavenlyBody("Moon of Venus", 27);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+
+        temp = new HeavenlyBody("Earth", 365);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        tempMoon = new HeavenlyBody("Moon of Earth", 27);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+
+        System.out.println("Planets");
+        for (HeavenlyBody planet: planets) {
+            System.out.println("\t" + planet.getName());
+        }
+
+        HeavenlyBody body = solarSystem.get("Mercury");
+        System.out.println("Moons of " + body.getName());
+        for (HeavenlyBody earthMoons : body.getSatellites()) {
+            System.out.println("\t" + earthMoons.getName());
+        }
+
+        Set<HeavenlyBody> moons = new HashSet<>();
+        for (HeavenlyBody planet: planets) {
+            moons.addAll(planet.getSatellites());
+        }
+
+        System.out.println("All moons");
+        for (HeavenlyBody moon: moons) {
+            System.out.println("\t" + moon.getName());
+        }
+    }
+}
